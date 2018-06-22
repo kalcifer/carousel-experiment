7c145a56f7e190a7d70cefcb494bb10dc645c390
pkodmad <pkodmad@atlassian.com>
2018-06-22 15:01:18 +1000

    Add hover functionality

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,6 +1,17 @@
 import React, { Component } from "react";
 import "./Carousel.css";
 
+const childHoverVals = {
+  transform: "scale(1.2)"
+};
+
+let childBeforeHoverVals = {
+  transform: "translateX(-10px)"
+};
+let childAfterHoverVals = {
+  transform: "translateX(10px)"
+};
+
 const padding = 10;
 export default class Carousel extends Component {
   state = {
@@ -10,7 +21,8 @@ export default class Carousel extends Component {
     position: 0,
     fullWidth: null,
     prev: false,
-    next: false
+    next: false,
+    hoverTransitionKey: null
   };
   constructor() {
     super();
@@ -32,7 +44,8 @@ export default class Carousel extends Component {
   shouldComponentUpdate(nextProps, nextState) {
     if (
       this.state.height === nextState.height &&
-      this.state.position === nextState.position
+      this.state.position === nextState.position &&
+      this.state.hoverTransitionKey === nextState.hoverTransitionKey
     ) {
       return false;
     }
@@ -40,71 +53,120 @@ export default class Carousel extends Component {
   }
   prev = () => {
     if (this.state.position !== 0) {
-      console.log("Before prev - ", this.state.position);
-      this.setState(
-        {
-          position: this.state.position - this.state.width - padding,
-          prev: true,
-          next: false
-        },
-        () => console.log("after prev - ", this.state.position)
-      );
+      this.setState({
+        position: this.state.position - this.state.width - padding,
+        prev: true,
+        next: false
+      });
     }
   };
   next = () => {
-    console.log("Before next - ", this.state.position);
-    this.setState(
-      {
-        position: this.state.position + this.state.width + padding,
-        prev: false,
-        next: true
-      },
-      () => console.log("after next - ", this.state.position)
-    );
+    this.setState({
+      position: this.state.position + this.state.width + padding,
+      prev: false,
+      next: true
+    });
+  };
+  onMouseEnter = key => {
+    this.setState({
+      hoverTransitionKey: key
+    });
+  };
+  onMouseLeave = key => {
+    this.setState({
+      hoverTransitionKey: null
+    });
   };
   render() {
     const { children } = this.props;
+    const {
+      hoverTransitionKey,
+      height,
+      position,
+      parentWidth,
+      fullWidth,
+      width
+    } = this.state;
+
+    let prevKey;
+    let nextKey;
     const count = React.Children.count(children);
     if (!children || count < 0) {
       return <div>Gimme some children</div>;
     }
-    if (!this.state.height) {
+    if (!height) {
       const firstChild = React.Children.toArray(children)[0];
       return <div ref={this.refDOM}>{firstChild}</div>;
     }
-    let translateValue = 0;
-    if (this.state.next) {
-      translateValue = -this.state.position;
-    }
-    if (this.state.prev) {
-      translateValue = -this.state.position;
+
+    const translateValue = -position;
+    if (hoverTransitionKey) {
+      let prevObj;
+      let currentObj;
+      React.Children.forEach(children, child => {
+        if (currentObj && !nextKey) {
+          nextKey = child.key;
+        }
+        if (child.key === hoverTransitionKey) {
+          if (prevObj) {
+            prevKey = prevObj.key;
+          }
+          currentObj = child;
+        }
+        prevObj = child;
+      });
     }
     return (
-      <div className="container" style={{ width: this.state.parentWidth }}>
+      <div className="container" style={{ width: parentWidth }}>
         <div
           className="carousel"
           style={{
-            height: this.state.height,
-            width: this.state.fullWidth,
+            height,
+            width: fullWidth,
             transform: `translateX(${translateValue}px)`,
             transition: "1s"
           }}
         >
-          {React.Children.map(children, child => (
-            <div
-              className="carousel-obj"
-              style={{ height: this.state.height, width: this.state.width }}
-            >
-              {child}
-            </div>
-          ))}
+          {React.Children.map(children, child => {
+            let childStyles = {};
+            const { key } = child;
+            if (key === hoverTransitionKey) {
+              childStyles = childHoverVals;
+            }
+            if (key === prevKey) {
+              childStyles = childBeforeHoverVals;
+            }
+            if (key === nextKey) {
+              childStyles = childAfterHoverVals;
+            }
+            childStyles = {
+              ...childStyles,
+              transition: "0.5s",
+              cursor: "pointer"
+            };
+            return (
+              <div
+                className="carousel-obj"
+                style={{ height, width, ...childStyles }}
+                onMouseEnter={() => this.onMouseEnter(key)}
+                onMouseLeave={() => this.onMouseLeave(key)}
+              >
+                {child}
+              </div>
+            );
+          })}
         </div>
-        <button disabled={this.state.position === 0} onClick={this.prev}>
+        <button
+          disabled={this.state.position === 0 || this.state.hoverTransitionKey}
+          onClick={this.prev}
+        >
           Prev
         </button>
         <button
           disabled={
-            this.state.position > this.state.fullWidth - this.state.parentWidth
+            this.state.position >
+              this.state.fullWidth - this.state.parentWidth ||
+            this.state.hoverTransitionKey
           }
           onClick={this.next}
         >
